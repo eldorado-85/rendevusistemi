16a8e9adc760456336ad558a73ddcf7d40c5ca5e
cagri fidan <a.cagrifidan@gmail.com>
2018-03-24 15:13:26 +0300

    layout ta Bir kaç dokunuş , control ve view lerin ayrılması

---
+++ rendevusistemi/Controllers/JobController.cs
@@ -0,0 +1,62 @@
+﻿using rendevusistemi.Database;
+using rendevusistemi.Database.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rendevusistemi.Controllers
+{
+    public class JobController : Controller
+    {
+
+        public ActionResult Job()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Job(Job myjob)
+        {
+            MyDbContext db = new MyDbContext();
+            db.Jobs.Add(myjob);
+            db.SaveChanges();
+            return RedirectToAction("JobView");
+
+
+        }
+
+        public ActionResult JobView()
+        {
+            MyDbContext db = new MyDbContext();
+            var list = db.Jobs.ToList();
+
+
+            return View(list);
+        }
+
+        public ActionResult JobEdit(int id)
+        {
+            MyDbContext db = new MyDbContext();
+            var list = db.Jobs.Where(a => a.Id == id).FirstOrDefault();
+
+
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult JobEdit(Job UpdateJob)
+        {
+            MyDbContext db = new MyDbContext();
+
+            var update = db.Jobs.Where(a => a.Id == UpdateJob.Id).FirstOrDefault();
+            update.Name = UpdateJob.Name;
+            db.SaveChanges();
+
+
+            return RedirectToAction("JobView");
+        }
+    }
+}
---
+++ rendevusistemi/Controllers/RegisterController.cs
@@ -0,0 +1,63 @@
+﻿using rendevusistemi.Database;
+using rendevusistemi.Database.Data;
+using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rendevusistemi.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Emploies emp)
+        {
+
+            using (MyDbContext db = new MyDbContext())
+            {
+
+                db.Employes.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("RegisterView");
+            }
+
+
+        }
+        public ActionResult RegisterView()
+        {
+            MyDbContext db = new MyDbContext();
+            List<Emploies> emp = db.Employes.ToList();
+
+
+            return View(emp);
+        }
+
+        public ActionResult RegisterEdit(int id)
+        {
+            MyDbContext db = new MyDbContext();
+            var edit = db.Employes.Where(a => a.Id == id).FirstOrDefault();
+
+            return View(edit);
+        }
+        [HttpPost]
+        public ActionResult RegisterEdit(Emploies UpdEmp)
+        {
+            MyDbContext db = new MyDbContext();
+            var update = db.Employes.Where(a => a.Id == UpdEmp.Id).FirstOrDefault();
+            update.Lastname = UpdEmp.Lastname;
+            update.Firstname = UpdEmp.Lastname;
+            update.Adress = UpdEmp.Adress;
+            update.Number = UpdEmp.Number;
+            db.SaveChanges();
+            return RedirectToAction("RegisterView");
+        }
+
+    }
+}
