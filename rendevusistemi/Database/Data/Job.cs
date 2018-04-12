d7355ed160710c6e8b565e9680512a39d6d79473
cagri fidan <a.cagrifidan@gmail.com>
2018-04-12 22:38:31 +0300

    CAN

--- rendevusistemi/Database/Data/Job.cs
+++ rendevusistemi/Database/Data/Job.cs
@@ -9,6 +9,7 @@ namespace rendevusistemi.Database.Data
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string LastName { get; set; }
 
         
 
