80bb1a91e9e8c036a130876a4fbed86bb67c2612
cagri fidan <a.cagrifidan@gmail.com>
2018-05-04 00:28:31 +0300

    login controller

---
+++ rendevusistemi/Database/Data/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace rendevusistemi.Database.Data
+{
+    public class Login
+    {    [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adını Giriniz.")]
+        [Display(Name = "Kullanıcı Adı")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+    }
+}
